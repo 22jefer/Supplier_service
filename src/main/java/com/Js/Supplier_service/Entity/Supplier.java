c5bb2cd83002ef2ed14ea3a4093b88b0fe7074bb
22jefer <jefferson02830@gmail.com>
2023-08-18 16:19:07 -0500

    Master

---
+++ src/main/java/com/Js/Supplier_service/Entity/Supplier.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Js.Supplier_service.Entity;
+
+/**
+ *
+ * @author Asus
+ */
+public class Supplier {
+    
+    
+    
+}
